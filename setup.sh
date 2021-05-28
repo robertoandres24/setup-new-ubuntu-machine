5dfda4105b4b0c000090e3b81b9b2fff8078aef0
robertoandres24 <robertoandres24@gmail.com>
2021-05-28 11:27:42 -0400

    add gcp scripts

--- setup.sh
+++ setup.sh
@@ -6,6 +6,11 @@ sudo apt install curl wget git stow tree dconf-editor unzip software-properties-
 # Node with Nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 nvm install --lts
+# Zsh and Oh-my-zsh
+sudo apt-get install zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+chsh -s `which zsh`
 # Docker 
 # https://docs.docker.com/engine/install/ubuntu/
 sudo apt-get remove docker docker-engine docker.io containerd runc
@@ -30,6 +35,16 @@ sudo apt install php7.1 php7.1-fpm php7.1-cli php7.1-zip php7.1-xml php7.1-soap 
 # Composer
 wget -O composer-setup.php https://getcomposer.org/installer
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+### GCP 
+# Kubernetes
+sudo snap install kubectl --classic
+# Gcloud
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update && sudo apt-get install google-cloud-sdk
+gcloud init
+gcloud container clusters get-credentials med360-app-cluster --zone us-central1-a --project med360-207318
+
 ### get main apps
 # Vscode
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
@@ -50,10 +65,11 @@ sudo rm /etc/apt/sources.list.d/google-chrome.list
 sudo snap install spotify
 # Simplenote
 sudo snap install simplenote
-# Zsh and Oh-my-zsh
-sudo apt-get install zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-chsh -s `which zsh`
 # Dbeaver
 sudo snap install dbeaver-ce
+# Slack
+sudo snap install slack --classic
+# Flameshot
+sudo snap install flameshot
+# Postman
+sudo snap install postman
